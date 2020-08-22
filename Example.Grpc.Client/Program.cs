a423682a898392952b6a24d0061abfb61cd9f0d8
Paweł Klatt <klatt.pawel@gmail.com>
2020-08-23 01:41:10 +0200

    Add client configuration

--- Example.Grpc.Client/Program.cs
+++ Example.Grpc.Client/Program.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace Example.Grpc.Client
 {
     static class Program
     {
-        static void Main(string[] args)
+        private static IConfiguration _configuration;
+        public static IConfiguration Configuration => _configuration ??= InitConfiguration();
+
+        private static IConfiguration InitConfiguration()
+        {
+            return new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        }
+
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
